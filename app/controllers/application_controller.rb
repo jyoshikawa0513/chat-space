22f2846e664b9ede3bf013111604fb0b5cdbf8f7
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-08-31 08:33:04 +0900

    configure name to strong_parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   config.generators do |g|
     g.stylesheets false
@@ -9,4 +10,7 @@ class ApplicationController < ActionController::Base
     g.test_framework false
   end
   
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
