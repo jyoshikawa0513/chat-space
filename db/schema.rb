37d8d2926dca4e10f6d75e869a2b246b4c4475f7
jyoshikawa0513 <jawa513@yahoo.co.jp>
2019-09-29 21:55:31 +0900

    change groupuser

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20190902084239) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group__users_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_group__users_on_user_id", using: :btree
+    t.index ["group_id"], name: "index_group_users_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_group_users_on_user_id", using: :btree
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
